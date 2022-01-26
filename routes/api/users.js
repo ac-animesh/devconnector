9550136c17df807ae98310cf771125fd8a87dde9
Animesh Chandra <animeshchandra01@gmail.com>
2022-01-27 04:09:26 +0530

    2nd Update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const { check,validationResult} = require('express-validator');
 const router = express();
 
-router.get('/', (req,res)=>{
+router.post('/', [
+    check('name','Name is required').not().isEmpty(),
+    check('email','pleas enter valid email').isEmail(),
+    check('password','password should be more than 6 char').isLength({min:6}),
+], (req,res)=>{
+    
+    const error = validationResult(req);
+
+    if(!error.isEmpty()){
+        return res.status(400).json({error: error.array()});
+    }
+    console.log(req.body);
     res.send("User route");
 })
 
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-
- connectDB();
-
 const PORT = process.env.PORT ||'5000';
+
+connectDB();
+
+app.use(express.json({extended:false}));
 
 app.get('/' , (req,res)=>{
     res.send('Welcome Everyone');
